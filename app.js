8a50f745bb4b69693bbb5c96cd741e05078040eb
Sunwoo <dlatjsdn205@Naver.com>
2022-10-13 17:13:41 +0900

    [ADD] Upload  File/ Canvas Text / CSS

--- app.js
+++ app.js
@@ -2,30 +2,35 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const button = document.querySelector("#paintBtn");
 const lineWidth = document.querySelector("#lineWidth");
-const lineColor = document.querySelector("#color-input");
+const colorInput = document.querySelector("#color-input");
+const modeBtn = document.querySelector("#mode-btn");
 const colorOptions = Array.from(
   document.getElementsByClassName("color-option")
 );
+const clearBtn = document.querySelector("#clear-btn");
+const eraserBtn = document.querySelector("#eraser-btn");
+const fileInput = document.getElementById("file");
+const textInput = document.getElementById("text");
+const saveBtn = document.getElementById("save-btn");
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 
 let isPainting = false;
+let isFilling = false;
 
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 
 function onMove(event) {
   if (isPainting) {
-    if (button.classList.contains("pen")) {
-      ctx.lineTo(event.offsetX, event.offsetY);
-      ctx.stroke();
-    } else if (button.classList.contains("fill")) {
-      ctx.fill();
-      ctx.stroke;
-    }
-
-    return;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
   }
-  ctx.beginPath();
   ctx.moveTo(event.offsetX, event.offsetY);
+  return;
 }
 
 function onMouseDown() {
@@ -33,18 +38,7 @@ function onMouseDown() {
 }
 function onMouseUp() {
   isPainting = false;
-}
-
-function changeBtn() {
-  if (button.classList.contains("pen")) {
-    button.classList.remove("pen");
-    button.classList.add("fill");
-    button.innerText = "fill";
-  } else {
-    button.classList.remove("fill");
-    button.classList.add("pen");
-    button.innerText = "pen";
-  }
+  ctx.beginPath();
 }
 
 function changeLineWidth(event) {
@@ -52,23 +46,88 @@ function changeLineWidth(event) {
   ctx.lineWidth = lineValue;
 }
 
+function colorChange(value) {
+  ctx.strokeStyle = value;
+  ctx.fillStyle = value;
+}
+
 function onColorChange(event) {
-  const pickedColor = event.target.value;
-  ctx.strokeStyle = pickedColor;
+  colorChange(event.target.value);
 }
 
 function onColorClick(event) {
-  const colorInfo = event.target.dataset.color;
-  ctx.strokeStyle = colorInfo;
+  const colorValue = event.target.dataset.color;
+  colorChange(colorValue);
+  colorInput.value = colorValue;
 }
 
-console.log(colorOptions);
+function modeChange() {
+  if (isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+function onCanvasClick() {
+  if (isFilling) {
+    ctx.fillStyle = colorInput.value;
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+function onClear() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
 
+function onErase() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  modeBtn.innerText = "Fill";
+}
+
+function fileUpload(event) {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image(); // document.createElement("img")
+  image.src = url;
+
+  //eventListener의 다른방식
+  // == image.addEventListener("load", function)
+  image.onload = function () {
+    ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    fileInput.value = null;
+  };
+}
+function ondbClick(event) {
+  const text = textInput.value;
+  ctx.save();
+  ctx.font = "60px solid";
+  ctx.lineWidth = 2;
+  ctx.strokeText(text, event.offsetX, event.offsetY);
+  ctx.restore();
+}
+
+function onSaveClick() {
+  const url = canvas.toDataURL();
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "myDrawing.png"; //a의 attribute 저장 이름 설정
+  a.click();
+}
+
+canvas.addEventListener("dblclick", ondbClick);
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mouseleave", onMouseUp);
-button.addEventListener("click", changeBtn);
+canvas.addEventListener("click", onCanvasClick);
 lineWidth.addEventListener("change", changeLineWidth);
-lineColor.addEventListener("change", onColorChange);
+colorInput.addEventListener("change", onColorChange);
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+modeBtn.addEventListener("click", modeChange);
+clearBtn.addEventListener("click", onClear);
+eraserBtn.addEventListener("click", onErase);
+fileInput.addEventListener("change", fileUpload);
+saveBtn.addEventListener("click", onSaveClick);
